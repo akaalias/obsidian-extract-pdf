352669dc91644d95eeeae65c48cda7d06ba8f0e3
Alexis Rondeau <alexis.rondeau@gmail.com>
2020-12-21 16:40:20 +0100

    Removes log statements

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ import worker from 'node_modules/pdfjs-dist/build/pdf.worker.entry';
 
 addIcon('target', '<path d="M50 88C29.0132 88 12 70.9868 12 50C12 29.0132 29.0132 12 50 12C70.9868 12 88 29.0132 88 50C87.9761 70.9769 70.9769 87.9761 50 88ZM50 22.8571C35.0094 22.8571 22.8571 35.0094 22.8571 50C22.8571 64.9906 35.0094 77.1429 50 77.1429C64.9906 77.1429 77.1429 64.9906 77.1429 50C77.1429 35.0094 64.9906 22.8571 50 22.8571ZM50 66.2857C41.0056 66.2857 33.7143 58.9943 33.7143 50C33.7143 41.0056 41.0056 33.7143 50 33.7143C58.9943 33.7143 66.2857 41.0056 66.2857 50C66.2857 54.3192 64.5699 58.4616 61.5157 61.5157C58.4616 64.5699 54.3192 66.2857 50 66.2857Z" fill="#646464"/>')
 
-var finalText = "";
-
 export default class ExtractPDFPlugin extends Plugin {
 
 	async onload() {
@@ -39,7 +37,6 @@ export default class ExtractPDFPlugin extends Plugin {
 					// @ts-ignore
 					.map(page => page.items.join('\n') + '\n')
 					.join('')
-				finalText = text;
 
 				return text;
 			});
