6345faf61a5b93fcc9bee4cafbec47e720bbce6e
Alexis Rondeau <alexis.rondeau@gmail.com>
2020-12-22 10:45:30 +0100

    Remove odd second settings file

--- src/ExtractPDFSettings 2.ts
+++
@@ -1,9 +0,0 @@
-export default class ExtractPDFSettings {
-    public createNewFile: boolean;
-    public copyToClipboard: boolean;
-
-    constructor() {
-        this.createNewFile = true;
-        this.copyToClipboard = true;
-    }
-}
